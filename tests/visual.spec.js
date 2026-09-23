2f3e02bd2d547eee1a8114567de11584696c721a
Vantikus <88005984+Vantikus@users.noreply.github.com>
2026-09-23 16:20:26 +0500

    Accept verified P2 workout visual baseline

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -10,7 +10,7 @@ const baseState={
 
 const visualHashes=Object.freeze({
   home:'02021377ce78e9dc3dec40a453f1ceb20bfcbbd7384ccb7641ce2e4300731099',
-  workout:'af701387f338fed56677dea37a97e2babc36c711d8e3e9368d84bbbebfc21f34',
+  workout:'7225427ba9d598af0b726c28a4333cd52a52e66357d9c5e33ba62a543029bf71',
   progress:'08a6f3343021f986158eb43a1e2c737031fd96bf6f20d62e0825da61a1ce9d5e'
 });
 
